e9c59a5985a7b18b18077955e06d4a8eb2b9739a
SOMI <som2mos@naver.com>
2024-03-06 22:06:08 +0900

    css 수정 / 포트폴리오페이지 조금

--- src/Comp/Portfolio.js
+++ src/Comp/Portfolio.js
@@ -1,7 +1,19 @@
 function Portfolio(){
     return(
         <section className="Portfolio">
-            <h1>포폴자리</h1>
+            <div className="container">
+                 <div className="mockup">
+                    <h2>이미지자리</h2>
+                 </div>
+                <div className="info">
+                    <h2>포트폴리오제목</h2>
+                    <p>여기는설명자리입니다</p>
+
+                    <div className="stack">
+                        <span>#Figma</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
